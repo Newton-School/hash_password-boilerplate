b5794ede2ba0150b9692e5b5997ac2e74e6940d9
MannShah17 <mannshah2004@gmail.com>
2023-11-18 17:29:48 +0530

    updated exports in boilerplate

--- controllers/hashController.js
+++ controllers/hashController.js
@@ -4,18 +4,14 @@ const User = require('../models/userModel');
 const router = express.Router();
 
 // TODO: Implement user registration logic with password hashing
-const registerUser = async (req, res) => {
+const hashedPassword = async (req, res) => {
   try {
-    const { username, password } = req.body;
-
     // TODO: Hash the password using bcrypt
     // TODO: Create a new user with the hashed password
-
   } catch (error) {
-    // TODO: Handle errors and respond with an appropriate status code and message
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
 
-module.exports = { registerUser };
+module.exports = { hashedPassword };
